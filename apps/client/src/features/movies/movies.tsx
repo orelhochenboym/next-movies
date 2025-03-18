a067d00e245fadbe0d7ef2b6b4ea2248cba42bb0
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2025-03-18 04:02:37 +0200

    feat(movies): handle gap between movies

--- apps/client/src/features/movies/movies.tsx
+++ apps/client/src/features/movies/movies.tsx
@@ -4,7 +4,7 @@ import { useFindAllMoviesQuery } from '../../store/services/api';
 export const Movies: React.FC = () => {
   const { data: movies } = useFindAllMoviesQuery();
   return (
-    <div className="grid grid-cols-6 items-center justify-center gap-4 px-36">
+    <div className="grid grid-cols-6 items-center justify-center gap-x-4 gap-y-20 px-36">
       {movies?.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}

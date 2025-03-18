5cc9133d48ac20eccd6ba70f5c841c7ec719dfeb
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2025-03-18 13:19:58 +0200

    refactor(social-media): links

--- apps/client/src/features/footer/components/social-media/social-media.tsx
+++ apps/client/src/features/footer/components/social-media/social-media.tsx
@@ -3,15 +3,33 @@ import linkedInIcon from '../../../../assets/linkedin-share-icon.svg';
 import twitterIcon from '../../../../assets/twitter-share-icon.svg';
 import instagramIcon from '../../../../assets/instagram-share-icon.svg';
 import youtubeIcon from '../../../../assets/youtube-share-icon.svg';
+import { NavLink } from 'react-router';
+import { Button } from '../../../../components/button';
 
 export const SocialMedia: React.FC = () => {
+  const buttons: Pick<React.ComponentProps<'img'>, 'alt' | 'src'>[] = [
+    { src: facebookIcon, alt: 'Facebook' },
+    { src: linkedInIcon, alt: 'LinkedIn' },
+    { src: twitterIcon, alt: 'Twitter' },
+    { src: instagramIcon, alt: 'Instagram' },
+    { src: youtubeIcon, alt: 'Youtube' },
+  ];
+
   return (
-    <div className="flex gap-8">
-      <img src={facebookIcon} alt="Facebook" />
-      <img src={linkedInIcon} alt="LinkedIn" />
-      <img src={twitterIcon} alt="Twitter" />
-      <img src={instagramIcon} alt="Instagram" />
-      <img src={youtubeIcon} alt="Youtube" />
+    <div className="flex items-center justify-center gap-4">
+      {buttons.map((button) => (
+        <NavLink
+          to="about:blank"
+          target="_blank"
+          rel="noreferrer"
+          tabIndex={-1}
+          key={button.alt}
+        >
+          <Button className="h-fit w-fit rounded-none border-none p-2 hover:bg-transparent">
+            <img src={button.src} alt={button.alt} />
+          </Button>
+        </NavLink>
+      ))}
     </div>
   );
 };

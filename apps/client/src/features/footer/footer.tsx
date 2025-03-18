0cfc23c05bb6a905f5b7182e887b0d5483f5b7d3
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2025-03-18 13:23:56 +0200

    refactor(footer): linked email address

--- apps/client/src/features/footer/footer.tsx
+++ apps/client/src/features/footer/footer.tsx
@@ -1,7 +1,10 @@
+import { NavLink } from 'react-router';
 import icon from '../../assets/icon.svg';
 import { SocialMedia } from './components/social-media';
 
 export const Footer: React.FC = () => {
+  const emailAddress = 'support@nextmovies.com';
+
   return (
     <div className="bg-background-footer flex h-fit w-full flex-col items-center justify-center gap-4 py-8 font-[Georgia] text-white">
       <img src={icon} alt="Next Movies Icon" />
@@ -10,7 +13,7 @@ export const Footer: React.FC = () => {
         <h1 className="text-4xl">Contact us</h1>
 
         <div className="flex h-fit w-fit flex-col items-center justify-center">
-          <p>support@nextmovies.com</p>
+          <NavLink to={`mailto:${emailAddress}`}>{emailAddress}</NavLink>
           <p>Mon - Fri | 6:00am - 5:00pm PT</p>
         </div>
 

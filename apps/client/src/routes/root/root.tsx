8f08da3d738f003a9a9a84d8559c84afcdb36b36
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2025-03-18 02:31:43 +0200

    feat(root): hero section

--- apps/client/src/routes/root/root.tsx
+++ apps/client/src/routes/root/root.tsx
@@ -1,20 +1,12 @@
 import { Footer } from '../../features/footer';
 import { Header } from '../../features/header';
+import { Hero } from '../../features/hero';
 
 export const Root: React.FC = () => {
   return (
     <div className="bg-background flex h-full w-full flex-col justify-between overflow-y-auto overflow-x-hidden">
       <Header />
-
-      <div className="flex h-fit w-full flex-col items-center justify-center font-[Arial] text-[5rem] font-bold">
-        <h1 className="animate-in slide-in-from-left-full duration-700 ease-out">
-          EXPLORE YOUR NEXT
-        </h1>
-        <h1 className="animate-in slide-in-from-right-full duration-700 ease-out">
-          MOVIES AND TV SHOWS
-        </h1>
-      </div>
-
+      <Hero />
       <Footer />
     </div>
   );
